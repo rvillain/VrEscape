46a5c58af134065d47c23e0a336a098201ace49c
Rémy Villain <remy.villain@apollossc.com>
2019-10-24 18:29:44 +0200

    correction super hands

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import 'jquery';
 import 'aframe';
+import 'aframe-extras';
+import 'aframe-event-set-component';
 import 'aframe-template-component';
 import 'aframe-geometry-merger-component';
-import 'aframe-event-set-component';
 import 'aframe-teleport-controls';
 import 'aframe-physics-system';
-import 'aframe-area-light-component';
+import 'aframe-physics-extras';
 import 'super-hands';
 import { GameManager } from './game-manager';
 import './components/buff-color';
